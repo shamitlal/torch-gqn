81800801dba1dd14c1a7fac1816c53d2894ecd68
shamit <shamitlal@yahoo.com>
2020-08-10 05:08:55 -0400

    Added some test scripts

---
+++ test/check_rot2eul.py
@@ -0,0 +1,45 @@
+import torch
+import pickle
+import numpy as np
+import ipdb 
+st = ipdb.set_trace
+
+def rotm2eul(r):
+    # r is Bx3x3
+    r00 = r[:,0,0]
+    r10 = r[:,1,0]
+    r11 = r[:,1,1]
+    r12 = r[:,1,2]
+    r20 = r[:,2,0]
+    r21 = r[:,2,1]
+    r22 = r[:,2,2]
+    
+    ## python guide:
+    # if sy > 1e-6: # singular
+    #     x = math.atan2(R[2,1] , R[2,2])
+    #     y = math.atan2(-R[2,0], sy)
+    #     z = math.atan2(R[1,0], R[0,0])
+    # else:
+    #     x = math.atan2(-R[1,2], R[1,1])
+    #     y = math.atan2(-R[2,0], sy)
+    #     z = 0
+    
+    sy = torch.sqrt(r00*r00 + r10*r10)
+    
+    cond = (sy > 1e-6)
+    rx = torch.where(cond, torch.atan2(r21, r22), torch.atan2(-r12, r11))
+    ry = torch.where(cond, torch.atan2(-r20, sy), torch.atan2(-r20, sy))
+    rz = torch.where(cond, torch.atan2(r10, r00), torch.zeros_like(r20))
+
+    # rx = torch.atan2(r21, r22)
+    # ry = torch.atan2(-r20, sy)
+    # rz = torch.atan2(r10, r00)
+    # rx[cond] = torch.atan2(-r12, r11)
+    # ry[cond] = torch.atan2(-r20, sy)
+    # rz[cond] = 0.0
+    return rx, ry, rz
+
+a = pickle.load(open("multi_obj_480_a_15905220985305424.p","rb"))
+extr = torch.tensor(a['origin_T_camXs_raw'])
+rx,ry,rz = rotm2eul(extr)
+
